df0ebf41b03af6b4f436e2c4693c6eb241d11171
Luis Octavio <octaviodevtech@gmail.com>
2024-10-20 00:35:31 -0600

    feat: iniciando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Home } from './pages/home/home';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
